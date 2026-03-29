f0b760688c9daf4556d62abac4438102a6ab23fb
Kiyo Takamatsu <kiotopkop@gmail.com>
2026-03-29 18:42:22 +0800

    fix: update base URL in vite to match github repository

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/pipelint/',
+  base: '/cicd-pipeline-auditor/',
   server: {
     port: 8081,
     host: '0.0.0.0'
